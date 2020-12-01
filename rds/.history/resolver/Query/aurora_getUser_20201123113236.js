a085cdc6ee328992a5afecb5ff2082b5262be0f2
Marcelo Olivas <mfolivas@gmail.com>
2020-11-30 20:11:18 -0500

    adding encrypted serverless rds

---
+++ rds/.history/resolver/Query/aurora_getUser_20201123113236.js
@@ -0,0 +1,29 @@
+var common = require('../Common/aurora')
+const Client = require('serverless-mysql')
+exports.func = async (_, { uuid }) => {
+    var client = Client({
+        config: {
+            host: process.env.MYSQL_HOST,
+            database: process.env.DB_NAME,
+            user: process.env.USERNAME,
+            password: process.env.PASSWORD
+        }
+    })
+    await common.init(client)
+    var resp = await common.getUser(client, uuid);
+    client.quit()
+    return resp;
+}
+
+exports.users = async () => {
+    client = Client({
+        config: {
+            host: process.env.MYSQL_HOST,
+            database: process.env.DB_NAME,
+            user: process.env.USERNAME,
+            password: process.env.PASSWORD
+        }
+    })
+    const response = await client.query('select * from users')
+    return JSON.stringify(response)
+}
